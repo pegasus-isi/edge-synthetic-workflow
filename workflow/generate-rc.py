73f1cc48623f755556b92c2a1428c0ca7bd8478e
Ryan Tanaka <tanaka@isi.edu>
2021-11-19 22:06:53 +0000

    updated generate-rc for test setup

--- workflow/generate-rc.py
+++ workflow/generate-rc.py
@@ -4,8 +4,16 @@ from Pegasus.api import *
 
 rc = ReplicaCatalog()
 
+'''
 for f in Path("./inputs").iterdir():
     if f.name.endswith(".txt"):
         rc.add_replica(site="local", lfn=f.name, pfn=f.resolve())
+'''
+for i in range(1,3):
+    rc.add_replica(
+                site="edge", 
+                lfn="test{}.txt".format(i), 
+                pfn="http://10.100.100.105/~panorama/test-data/test{}.txt".format(i)
+        )
 
 rc.write()
