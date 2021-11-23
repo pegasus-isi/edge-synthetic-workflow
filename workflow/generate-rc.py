886d49c47c506d7071418c9c42259421660fe35e
Ryan Tanaka <tanaka@isi.edu>
2021-11-23 18:41:53 +0000

    refactor generate-rc to work with all scenarios

--- workflow/generate-rc.py
+++ workflow/generate-rc.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python3
+import argparse
+import sys
 from pathlib import Path
+from typing import List
 from Pegasus.api import *
 
-rc = ReplicaCatalog()
+def parse_args(args: List[str] = sys.argv[1:]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="generate rc....")
+    parser.add_argument(
+        "--compute-on-edge",
+        default=False,
+        action="store_true",
+        help="Sets pfns to file paths instead of http urls as we will symlink (initial jobs will run on edge so no need to pull inputs over http)"
+    )
 
-'''
-for f in Path("./inputs").iterdir():
-    if f.name.endswith(".txt"):
-        rc.add_replica(site="local", lfn=f.name, pfn=f.resolve())
-'''
-for i in range(1,3):
-    rc.add_replica(
-                site="edge", 
-                lfn="test{}.txt".format(i), 
-                pfn="http://10.100.100.109/~panorama/test-data/test{}.txt".format(i)
-        )
+    return parser.parse_args(args)
 
-rc.write()
+if __name__=="__main__":
+    args = parse_args()
+
+    rc = ReplicaCatalog()
+
+    NUM_INPUT_FILES = 8
+
+    for i in range(1, NUM_INPUT_FILES+1):
+        lfn="test{}.txt".format(i)
+
+        pfn = None
+        site = None
+        if args.compute_on_edge:
+            site = "condorpool"
+            pfn = "/home/panorama/public_html/test-data/{}".format(lfn)
+        else:
+            site = "edge"
+            pfn = "http://10.100.100.109/~panorama/test-data/{}".format(lfn)
+        
+        rc.add_replica(site=site, lfn=lfn, pfn=pfn)
+    
+    rc.write(sys.stdout)
+    rc.write()
+
