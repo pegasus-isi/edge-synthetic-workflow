91400e50b8016a27bedb82ac6a6215ec955da641
Ryan Tanaka <tanaka@isi.edu>
2021-11-24 15:59:58 +0000

    run scripts for cloud only and edge only experiment

---
+++ workflow/run-edge-only.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# generate site catalog
+python3 generate-sc.py --edge-only
+
+# generate replica catalog
+python3 generate-rc.py --compute-on-edge
+
+# run workflow
+for i in {1..10}
+do
+	echo "Doing run ${i}"
+
+	python3 workflow.py \
+	    --height 3 \
+	    --runtime 40 20 60 \
+	    --output-sizes 500 250 \
+	    --edge-only \
+	    --submit
+done
+
+# watch till completion
+#watch -n 1 pegasus-status -l $(cat submit_dir_path.txt)
