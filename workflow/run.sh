f74e5f4300d3fdaa9dc3cf44d7beedcfa743dc20
Ryan Tanaka <tanaka@isi.edu>
2021-11-24 19:42:13 +0000

    added master experiment run script

--- workflow/run.sh
+++ workflow/run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -e
-
-#python3 generate-edge-cloud-site-catalog.py
-python3 generate-edge-only-site-catalog.py
-
-python3 generate-rc.py
-
-python3 workflow.py \
-	--pegasus-keg-path /usr/bin/pegasus-keg \
-	--height 2 \
-	--runtime 20 20 \
-	--output-sizes 1024 2048 \
-	--replica-catalog replicas.yml \
-	--job-mapping \
-	--submit
-
-watch -n 1 pegasus-status -l $(cat submit_dir_path.txt) 
+./run-edge-cloud.sh
+./run-edge-only.sh
+./run-cloud-only.sh
