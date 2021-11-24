582d21cb8aa587be70bc3d8f9aa37e6cf758e635
Ryan Tanaka <tanaka@isi.edu>
2021-11-24 00:34:37 +0000

    added parsing of file xfer data from ptransfer output

--- data-parser/parse.py
+++ data-parser/parse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import pprint
+import shlex
 import sys
 from pathlib import Path
 from typing import List, Dict
@@ -58,8 +59,20 @@ def parse_job_files(out_file: Path, data: Dict) -> None:
     entry = {
             "job": out_file.name.split(".")[0],
             "kickstart-record": None,
-            "transfer-input": None,
-            "transfer-output": None
+            "transfer-input": {
+                    "output": None,
+                    "num-transfered": None,
+                    "amount-transfered": None,
+                    "duration": None,
+                    "rate": None,
+                },
+            "transfer-output": {
+                    "output": None,
+                    "num-transfered": None,
+                    "amount-transfered": None,
+                    "duration": None,
+                    "rate": None,
+                }
         }
 
     with out_file.open("r") as f:
@@ -73,31 +86,72 @@ def parse_job_files(out_file: Path, data: Dict) -> None:
 
     entry["kickstart-record"] = yaml.load("".join(ks_record), Loader=yaml.Loader)
 
-    err_file = Path(out_file.name.replace(".out.", ".err.")) 
-    err_file = out_file.resolve().parent / err_file
-    transfer_input = list()
-    transfer_output = list()
-    with err_file.open("r") as f:
-        while True:
-            l = f.readline()
+    def get_xfer_stats(line: str) -> dict:
+        tokens = shlex.split(line)
+        return {
+                "num-transfered": int(tokens[5]),
+                "amount-transfered": tokens[7] + " " + tokens[8],
+                "duration": tokens[11] + " " + tokens[12].replace(".", ""),
+                "rate": tokens[14] + " " + tokens[15]
+            }
 
-            if "Staging in input data and executables" in l:
-                while True:
-                    l = f.readline()
-                    if "[Pegasus Lite] Executing the user task" in l:
-                        break
-                    transfer_input.append(l)
+    if "register_staging" not in out_file.name and\
+            "cleanup_" not in out_file.name and\
+            "clean_up_" not in out_file.name and\
+            "create_dir" not in out_file.name and\
+            "stage_out" not in out_file.name:
+        err_file = Path(out_file.name.replace(".out.", ".err.")) 
+        err_file = out_file.resolve().parent / err_file
 
-            if "Staging out output files" in l:
-                while "PegasusLite: " not in l:
-                    l = f.readline()
-                    transfer_output.append(l)
+        transfer_input = list()
+        transfer_input_metrics = None
 
-            if l is None or l == "":
+        transfer_output = list()
+        transfer_output_metrics = None
+
+        with err_file.open("r") as f:
+            while True:
+                l = f.readline()
+
+                if "Staging in input data and executables" in l:
+                    while True:
+                        l = f.readline()
+                        if "[Pegasus Lite] Executing the user task" in l:
+                            break
+                        
+                        if "Stats: Total" in l:
+                            transfer_input_metrics = get_xfer_stats(l)
+
+                        transfer_input.append(l)
+
+                if "Staging out output files" in l:
+                    while "PegasusLite: " not in l:
+                        l = f.readline()
+
+                        if "Stats: Total" in l:
+                            transfer_output_metrics = get_xfer_stats(l)
+
+                        transfer_output.append(l)
+
+                if l is None or l == "":
+                    break
+        
+        entry["transfer-input"]["output"] = "\n".join(list(map(lambda s: s.strip(), transfer_input)))
+        entry["transfer-input"].update(transfer_input_metrics)
+
+        entry["transfer-output"]["output"] = "\n".join(list(map(lambda s: s.strip(), transfer_output)))
+        entry["transfer-output"].update(transfer_output_metrics)
+
+    if "stage_out" in out_file.name:
+        stage_out_xfer_output = entry["kickstart-record"][0]["files"]["stdout"]["data"]
+        transfer_output_metrics = None
+        for l in stage_out_xfer_output.split("\n"):
+            if "Stats: Total" in l:
+                transfer_output_metrics = get_xfer_stats(l)
                 break
-    
-    entry["transfer-input"] = "\n".join(list(map(lambda s: s.strip(), transfer_input)))
-    entry["transfer-output"] = "\n".join(list(map(lambda s: s.strip(), transfer_output)))
+
+        entry["transfer-output"]["output"] = stage_out_xfer_output
+        entry["transfer-output"].update(transfer_output_metrics)
 
     data[out_file.name.split(".")[0]] = entry
 
