db40a5a2d7efd552a3e555bbef5ff61174a40a51
Ryan Tanaka <tanaka@isi.edu>
2021-11-25 00:46:09 +0000

    updated start-worker script to spin up 24 workers and limit cpu time to 2/3

--- edge-node-utils/start-worker.sh
+++ edge-node-utils/start-worker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-docker container run \
-    --cpus=1 \
-    -e MACHINE_SPECIAL_ID=1 \
-    -e CONDOR_HOST=10.100.101.107 \
-    -e NUM_CPUS=1 \
-    --rm \
-    --detach \
-    --name edge-worker-$1 \
-    -v /home/panorama/public_html:/home/panorama/public_html \
-    ryantanaka/execute
+
+for i in {1..24}
+do
+        docker container run \
+            --cpus="0.67" \
+            -e MACHINE_SPECIAL_ID=1 \
+            -e CONDOR_HOST=10.100.101.107 \
+            -e NUM_CPUS=1 \
+            --rm \
+            --detach \
+            --name edge-worker-$i \
+            -v /home/panorama/public_html:/home/panorama/public_html \
+            ryantanaka/execute
+done
